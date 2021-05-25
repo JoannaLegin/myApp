f0e3677ccbcb57eb2cbe68b17dc7bd35289249a0
JoannaLegin <36193081+JoannaLegin@users.noreply.github.com>
2021-05-26 00:25:51 +0200

    Create Button.js

---
+++ src/Components/Button.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+class Button extends React.Component {
+    render() {
+        return (
+            <div>
+                <a href="" className="navbarMainBtn">START</a>
+            </div>
+        )
+    }
+}
+
+export default Button;
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+
+
+
+
